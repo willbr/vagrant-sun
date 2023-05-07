455f1015081f1c3cb044794c1e0acb8d6ddf3114
William Bettridge-Radford <william.bettridge.radford@gail.com>
2023-05-07 13:02:13 +0100

    wip floor

--- sketch2.py
+++ sketch2.py
@@ -1,46 +1,43 @@
 import pyxel
 import math
 
-x_offset = 16
-shear = 0
+shear_limit = 1
+shear_step = 0.1
+shear = shear_limit
 auto = True
 
-pyxel.init(320, 144, title="gameboy", fps=60)
+pyxel.init(320, 144, title="gameboy", fps=15)
 pyxel.camera(-80, 0)
 
 pyxel.image(0).load(0, 0, "images/background3.png")
-pyxel.image(0).load(0, 32, "images/actors.png")
 
 def update():
-    global x_offset
-    global shear
     global auto
+    global shear
 
-    if pyxel.btnp(pyxel.KEY_Q):
-        pyxel.quit()
+    if pyxel.btn(pyxel.KEY_LEFT):
+        shear -= shear_step
 
-    if pyxel.btnp(pyxel.KEY_LEFT):
-        x_offset -= 1
-
-    if pyxel.btnp(pyxel.KEY_RIGHT):
-        x_offset += 1
+    if pyxel.btn(pyxel.KEY_RIGHT):
+        shear += shear_step
 
     if pyxel.btnp(pyxel.KEY_SPACE):
         auto = not auto
 
     if auto:
-        x_offset -= 1
+        pass
 
-    x_offset %= (16 * 3)
-    shear = 0.05 * (x_offset - 16)
+    if auto:
+        shear -= shear_step
+
+    if shear < -shear_limit:
+        shear = shear_limit
+    elif shear > shear_limit:
+        shear = -shear_limit
 
 
 def draw():
     pyxel.cls(0)
-    #background()
-    #ceiling()
-    #actors()
-
 
     if False:
         pyxel.line(-16,0, -16,140, 2)
@@ -56,65 +53,26 @@ def draw():
         pyxel.rect(-80, 0, 80, 144, 3)
         pyxel.rect(160, 0, 80, 144, 3)
 
-    pyxel.rect(80, 80, 2, 2, 5)
+    #pyxel.rect(80, 80, 2, 2, 5)
 
-    pyxel.text(4,4, f"{x_offset}\n{shear:+2.2f}", 2)
-
-def background():
-    dst_x = (x_offset % 32) - 32
-    #dst_x = -32
-    dst_y = 32
-    for i in range(48):
-        pyxel.blt(dst_x, dst_y, 0, 0, 3, 224, 1)
-        dst_y += 1
+    pyxel.text(4,4, f"{shear:+2.2f}", 2)
 
 def floor():
     src_y = 0
     y = 80
+    x = (16 * shear) - 32
 
 
     for i in range(32):
-        x = (shear * i) + x_offset - 64
-        x /=  4
-        y += 1
         pyxel.blt(x, y, 0, 0, src_y, 224, 1)
         y += 1
         pyxel.blt(x, y, 0, 0, src_y, 224, 1)
+        y += 1
+
+        if y % 2 == 0:
+            x += shear
+
         src_y += 1
-
-    i = 0
-    x = (shear * i) + x_offset - 64
-    y = 80
-    pyxel.rect(x, y, 10, 10, 5)
-
-
-def ceiling():
-    dst_x = (x_offset % 32) - 32
-    dst_x = -32
-    dst_y = 32
-    src_y = 0
-    for i in range(64):
-        pyxel.blt(dst_x, dst_y, 0, 0, src_y, 224, 1)
-        dst_y -= 1
-        if i % 2 == 0:
-            src_y += 1
-
-def actors():
-    pyxel.blt(x_offset, 90,
-              0,
-              0, 32,
-              17, 32)
-
-    pyxel.blt(100, 40,
-              0,
-              24, 32,
-              17, 32)
-
-    pyxel.blt(130, 90,
-              0,
-              50, 32,
-              17, 32)
-
 
 pyxel.run(update, draw)
 
